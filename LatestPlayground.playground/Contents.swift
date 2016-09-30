dde67214ba0b239b0e4c3bcedf2836b3a754e5c0
Jim Publicover <jameslpublicover@gmail.com>
2016-09-30 13:15:47 -0400

    new playground for further turorial

---
+++ LatestPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+widthLabel
+
+//let label_again = label + width
+
+let new_var: Float = 4.0
+
+var occupations = [String: String]()
+
+occupations["me"] = "bird helper"
+
+occupations
